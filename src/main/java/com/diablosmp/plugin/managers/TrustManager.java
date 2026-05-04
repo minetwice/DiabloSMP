906e2bde064a955f411afc6188ebb0e8b44eeb34
Twicefear <ytd82774@gmail.com>
2026-05-04 12:43:17 +0530

    Create TrustManager scaffold

---
+++ src/main/java/com/diablosmp/plugin/managers/TrustManager.java
@@ -0,0 +1,11 @@
+package com.diablosmp.plugin.managers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class TrustManager {
+    // Basic trust logic will go here
+    public TrustManager() {
+    }
+}
