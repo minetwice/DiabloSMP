18d9368131e3e2d338baf55b833f2725e2e64ffc
Twicefear <ytd82774@gmail.com>
2026-05-04 12:43:16 +0530

    Create AbilityManager scaffold

---
+++ src/main/java/com/diablosmp/plugin/managers/AbilityManager.java
@@ -0,0 +1,11 @@
+package com.diablosmp.plugin.managers;
+
+import com.diablosmp.plugin.DiabloPlugin;
+
+public class AbilityManager {
+    private final DiabloPlugin plugin;
+
+    public AbilityManager(DiabloPlugin plugin) {
+        this.plugin = plugin;
+    }
+}
